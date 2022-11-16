250ca85b7d372267556f2e1e9e1cf3bb10af164b
nurilloh2004 <nurillohpython@gmail.com>
2022-11-16 20:01:48 +0500

    all

--- farm/models.py
+++ farm/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Settings(models.Model):
+    key = models.CharField(max_length=65)
+    value = models.CharField(max_length=550)
+
+    def __str__(self):
+        return self.key
+
+    class Meta:
+        verbose_name = 'Settings'    
+
+
+class 
